eee846bd741ec1f42b77dfc853319d12c2270834
Halil Can <54184509+halilcn@users.noreply.github.com>
2023-11-09 16:25:50 +0300

    feat: add error log for unexpected erorrs

--- src/errorHandler.js
+++ src/errorHandler.js
@@ -2,7 +2,10 @@ const { BadRequestError } = require("./errors");
 const { isCustomError } = require("./utils");
 
 module.exports = (err, req, res, next) => {
-  const { status, message, data } = isCustomError(err)
+  const _isCustomError = isCustomError(err);
+  if (!_isCustomError) console.error("Error:", err);
+
+  const { status, message, data } = _isCustomError
     ? err
     : new BadRequestError("An unknown error occurred");
 
