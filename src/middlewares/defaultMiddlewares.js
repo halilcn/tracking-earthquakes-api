503495d64165ac17427ae732ac34c25373a0e161
Halil Can <54184509+halilcn@users.noreply.github.com>
2023-11-10 16:50:39 +0300

    chore: delete cors settings

--- src/middlewares/defaultMiddlewares.js
+++ src/middlewares/defaultMiddlewares.js
@@ -3,15 +3,7 @@ const bodyParser = require("body-parser");
 const responseHandler = require("../responseHandler");
 
 module.exports = (app) => {
-  app.use(
-    cors({
-      origin: [
-        "https://tracking-earthquakes.vercel.app",
-        "http://localhost:5173/",
-      ],
-      methods: "GET,POST,PUT,DELETE,OPTIONS",
-    })
-  );
+  app.use(cors());
   app.use(bodyParser.json());
   app.use(responseHandler);
 };
