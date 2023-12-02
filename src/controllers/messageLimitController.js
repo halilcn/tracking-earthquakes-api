8d5420701e417d17cdb8af39504ba4fb54f3f2fb
Halil Can <54184509+halilcn@users.noreply.github.com>
2023-12-02 22:44:55 +0300

    ref: change response of message limits

--- src/controllers/messageLimitController.js
+++ src/controllers/messageLimitController.js
@@ -3,9 +3,11 @@ const MessageLimit = require("../models/messageLimit");
 exports.index = async (req, res, next) => {
   const userId = String(req.user._id);
 
-  const messageLimits = await MessageLimit.find({ user: userId });
+  const { token } = await MessageLimit.findOne({ user: userId });
 
   res.success({
-    messageLimits,
+    messageLimits: {
+      token,
+    },
   });
 };
